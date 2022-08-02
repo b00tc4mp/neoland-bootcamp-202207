af65aeeedcaac5bf13a980feeb76f78d83a1e25b
donyol <danieltomlinson123@gmail.com>
2022-08-02 17:46:24 +0200

    reduce Cachay with test 1 and 2, object file with .call(this) within constructor funciton

--- staff/daniel-tomlinson/Cachay/objects.js
+++ staff/daniel-tomlinson/Cachay/objects.js
@@ -1,75 +1,25 @@
-/* function Person(name, surname, age, password, nationality) {
-  this.name = name;
-  this.surname = surname;
-  this.age = age;
-  this.password = password;
-  this.nationality = nationality;
-
-  this.fullName = fullName(this.name, this.surname);
-}
-
-function fullName(firstName, surname) {
-  firstName + " " + surname;
-}
-
-const Mati = new Person("Mati", "Bua", undefined, "123", "Aregentina");
-
-console.log(Mati); */
-
 function Person(name, surname, age, password, nationality) {
-  //   "use strict";
+  //"use strict";
   this.name = name;
   this.surname = surname;
   this.age = age;
   this.password = password;
   this.nationality = nationality;
 
-  //   this.fullName = fullName();
-  this.nameFull = this.name + " " + this.surname;
+  this.fullName = completeName.call(this);
 
-  this.fullName2 = function () {
-    return this.firstName + " " + this.lastName;
-  };
+  // In this case can als writ the following:
+  // this.fullName = this.name + " " + this.surname
+  this.fullName1 = `${this.name} ${this.surname}`;
 }
-
-Person.prototype.fullName = function () {
-  const fullname = this.name + " " + this.surname;
-
-  return fullname;
-};
 
 function completeName() {
+  debugger;
   const fullname = this.name + " " + this.surname;
 
   return fullname;
 }
 
-Person.completeName = completeName();
+const person = new Person("Charly", "Brown", undefined, "123", "American");
 
-const mati = new Person("Mati", "Bua", undefined, "123", "Aregentina");
-
-const matiFullName = mati.fullName();
-
-console.log(mati);
-
-console.log(matiFullName);
-
-console.log(mati.fullName());
-
-console.log(mati.fullName2());
-
-/* const matias = {
-  name: "Mati",
-  age: 26,
-  password: "123",
-  nationality: "Aregentina",
-};
-
-console.log(matias); */
-
-/* const matt = Object.create();
-
-matt.name = "Matt";
-matt.age = 26;
-matt.password = "123";
-matt.nationality = "Argentina"; */
+console.log(person);
