225c504f1b299c274346183a988ab16d35e2428a
andrew.rojas13 <andrewrojas_4@hotmail.com>
2022-08-08 14:41:06 +0200

    add app update

---
+++ staff/andrew-rojas/strings/concat.js
@@ -0,0 +1,20 @@
+function concat(text, text2,) 
+    // TODO ...
+
+    var concat;
+
+    for ( var i = 0; i < concat.length; i++) {
+        concat = [i]
+
+    }
+
+// tests
+
+console.log(concat('Hola', 'Mundo'))
+// HolaMundo
+
+console.log(concat('Adios', 'Mundo', 'Cruel'))
+// AdiosMundoCruel
+
+console.log(concat('i', ' ', 'love', ' ', 'coding'))
+// i love coding
---
+++ staff/andrew-rojas/strings/count-words.js
@@ -0,0 +1,23 @@
+function countWords(text) {
+  // TODO ...
+
+  
+}
+
+// tests
+
+console.log(countWords("adiós mundo cruel"));
+// 3
+
+console.log(countWords("hello world "));
+// 2
+
+console.log(countWords("1 2 3 4 5"));
+// 5
+
+
+// {
+//   var result = text.split(" ");
+
+//   return result.filter((word) => word !== " ").length;
+// }
