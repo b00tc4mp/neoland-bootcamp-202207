c99b6c2e95d8934a86805122630a91d192e6c973
weird696969 <manuel.villalvilla@gmail.com>
2022-08-08 13:03:10 +0200

    nada

--- staff/manuel-villalvilla/arrays/flat.js
+++ staff/manuel-villalvilla/arrays/flat.js
@@ -2,13 +2,13 @@ function flat(array, depth) {
     if (!(array instanceof Array))
         throw new Error(`${array} not an array`)
      
-    if (depth && typeof depth === 'number') {
+    if (depth && typeof depth === 'number' && depth !== Infinity) {
         let newArr = flat(array); 
         for (let a = 1; a < depth; a++) {
             newArr = flat(newArr);
         }
         return newArr;
-    }
+    } 
     const newArray = []; 
     for (let i = 0; i < array.length; i++) {
         if (array[i] instanceof Array) {
