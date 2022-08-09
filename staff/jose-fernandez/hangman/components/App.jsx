3f820bd886be7016f7d0d71e3779960f1d1f787c
yoferpatlv <yoferpa_1995@hotmail.com>
2022-08-09 09:37:05 +0200

    react hangman

--- staff/jose-fernandez/hangman/components/App.jsx
+++ staff/jose-fernandez/hangman/components/App.jsx
@@ -1,16 +1,39 @@
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { view: 'select word', result: null }
+        this.state = { view: 'select word', result: null ,wordShown: null, counter: 10}
     }
 
     handleOnChooseWordFormSubmit = event => {
         event.preventDefault()
 
         const wordSelected = event.target.input.value
-        this.setState({ result: wordSelected, view: 'playing' })
+        this.setState({ result: wordSelected, view: 'playing',wordShown:"_".repeat(wordSelected.length) })
        
+    }
 
+    handleOnCharacterForm = event =>{
+        const charTried = event.target.input.value
+
+        event.target.reset()
+
+        //VERIFICO SI EL CARACTER ESTÁ EN EL RESULTADO
+        if(this.state.result.includes(charTried)){
+            //TRANSFORMO LA PALABRA MOSTRADA EN ARRAY
+            let newWordShown= this.state.wordShown.split('')
+
+            //LOCALIZO LOS INDICES EN LOS QUE ESTA EL CARACTER Y MODIFICO LA PALABRA MOSTRADA EN ESOS INDICES
+            for(let i=0;i<this.state.result.length;i++)
+                if(this.state.result[i]=== charTried)
+                    newWordShown[i] = charTried
+
+            //MODIFICO EL ESTADO
+            this.setState({wordShown: newWordShown.join('')})
+        }else{
+            const newCounter = this.state.counter-1
+
+            this.setState({counter: newCounter})
+        }
     }
 
     render() {
@@ -19,8 +42,8 @@ class App extends React.Component {
                 {this.state.view === "select word" && <Form placeholder="enter a word" onSubmit={this.handleOnChooseWordFormSubmit} buttonText="START" />}
 
                 {this.state.view === "playing" && <>
-                    <h1>{"_ ".repeat(this.state.result.length)}</h1>
-                    <Form placeholder="enter a character" buttonText="TRY" />
+                    <h1 className="wordShown">{this.state.wordShown}</h1>
+                    <Form placeholder="enter a character" buttonText="TRY" onSubmit={this.handleOnCharacterForm}/>
                    
                 </>
                 }
--- staff/jose-fernandez/hangman/components/Form.jsx
+++ staff/jose-fernandez/hangman/components/Form.jsx
@@ -1,6 +1,10 @@
 function Form(props) {
     return (
-        <form className="form" onSubmit={props.onSubmit}>
+        <form className="form" onSubmit={function(event){
+            event.preventDefault()
+
+            props.onSubmit(event)
+        }}>
             <input placeholder={props.placeholder} name="input" />
             <button type="submit">{props.buttonText}</button>
         </form>
