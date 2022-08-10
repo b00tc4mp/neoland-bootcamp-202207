359edace6b69a02d0db403fd5cf014a31470ff4f
manuelbarzi <manuelbarzi@gmail.com>
2022-08-10 13:43:53 +0200

    migrate app to react-app

---
+++ staff/manuel-barzi/react-app.1/components/react-with-state-logging.js
@@ -0,0 +1,9 @@
+const setState = React.Component.prototype.setState
+
+React.Component.prototype.setState = function(state) {
+    console.log('set state')
+
+    setState.call(this, state)
+
+    console.log('after set state')
+}
