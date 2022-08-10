540ab77e895a68b8cb6f2428910dfb1944d12b3a
manuelbarzi <manuelbarzi@gmail.com>
2022-08-10 11:22:47 +0200

    add logger to tictactoe

--- staff/manuel-barzi/tictactoe/components/App.jsx
+++ staff/manuel-barzi/tictactoe/components/App.jsx
@@ -3,9 +3,15 @@ class App extends React.Component {
         super()
 
         this.state = startGame()
+
+        this.logger = new Loggito('App')
+
+        this.logger.info('constructor')
     }
 
     handleCellClick = event => {
+        this.logger.debug('handle cell click')
+
         const cell = event.target
 
         const index = cell.dataset.index
@@ -15,9 +21,31 @@ class App extends React.Component {
         this.setState(state)
     }
 
-    handlePlayAgainClick = () => this.setState(startGame())
+    handlePlayAgainClick = () => {
+        this.logger.debug('handle play again click')
+
+        this.setState(startGame())
+    }
+
+    componentDidMount() {
+        this.logger.info('component did mount')
+    }
+
+    componentDidUnmount() {
+        this.logger.info('component did unmount')
+    }
+
+    setState(state) {
+        this.logger.info('set state')
+
+        super.setState(state)
+
+        this.logger.info('after set state')
+    }
 
     render() {
+        this.logger.info('render')
+
         return <div className="container">
             <Board board={this.state.board} onCellClick={this.handleCellClick} />
             <Result result={this.state.result} onPlayAgainClick={this.handlePlayAgainClick} />
