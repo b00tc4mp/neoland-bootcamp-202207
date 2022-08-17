689410fdc451a97f28b676bda6103ecdc642d923
donyol <danieltomlinson123@gmail.com>
2022-08-17 10:57:07 +0200

    hangman App and form format

--- staff/daniel-tomlinson/hangman_all/hangman/components/Form.jsx
+++ staff/daniel-tomlinson/hangman_all/hangman/components/Form.jsx
@@ -1,15 +1,17 @@
 function Form(props) {
-    return (
-        <form
-            className="form"
-            onSubmit={function (event) {
-                event.preventDefault()
-                
-                props.onSubmit(event)
-            }}>
+  return (
+    <form
+      className="form"
+      onSubmit={function (event) {
+        event.preventDefault();
 
-            <input placeholder={props.placeholder} name="input" autocomplete="off" maxlength="1"/>
-            <button type="submit">{props.buttonText}</button>
-        </form>
-    )
+        props.onSubmit(event);
+      }}
+    >
+      <input placeholder={props.placeholder} name="input" autocomplete="off" />
+      <button type="submit">{props.buttonText}</button>
+    </form>
+  );
 }
+
+// maxlength = "1";
