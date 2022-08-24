123d8d5f88468fb3ccae56ade072173d3021f6f2
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-08-24 15:55:37 +0200

    add errors module

---
+++ staff/adrian-ruiz/postits/errors/src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    AuthError: require('./AuthError'),
+    DuplicityError: require('./DuplicityError'),
+    RegexError: require('./RegexError'),
+    FormatError: require('./FormatError'),
+    SystemError: require('./SystemError'),
+    UnknownError: require('./UnknownError')
+}
