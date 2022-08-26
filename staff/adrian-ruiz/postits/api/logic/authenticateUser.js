875dbf0bb180d9013a389f03b733b723d30edcfe
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-08-26 10:10:27 +0200

    change logic to conect db with mongoose

--- staff/adrian-ruiz/postits/api/logic/authenticateUser.js
+++ staff/adrian-ruiz/postits/api/logic/authenticateUser.js
@@ -1,71 +1,23 @@
-const { readdir, readFile } = require('fs')
 const { validatePassword, validateEmail, validateCallback } = require('validators')
-const { AuthError, SystemError, UnknownError } = require('errors')
+const { AuthError, UnknownError } = require('errors')
+const User = require('../models/user')
 
-function authenticateUser(email, password, callback) {
+async function authenticateUser(email, password, callback) {
     //TODO validate inputs
     validatePassword(password)
     validateEmail(email)
     validateCallback(callback)
-
-    const folder = './data/users'
-
-    try {
-        readdir(folder, (error, files) => {
-            if (error) {
-                callback(new SystemError(`Can't list files from folder: ${folder}`))
-                return
-            }
-
-            files = files.filter(file => !file.startsWith('.'))
-
-            if (files.length === 0) {
-                callback(new AuthError('Email and/or password wrong'))
-                return
-            }
-
-            try {
-                let index = 0
-                let file = files[index];
-
-                (function iterate() {
-                    readFile(`./data/users/${file}`, 'utf8', (error, json) => {
-                        if (error) {
-                            callback(new SystemError(`Can't read file: ${file} in folder: ${folder}`))
-                            return
-                        }
-
-                        const user = JSON.parse(json)
-
-                        if (user.email === email) {
-                            if (user.password === password) {
-                                callback(null, user.id) 
-                                return
-                            }
-                            callback(new AuthError('Email and/or password wrong'))
-                            return
-                        }
-
-                        index++
-                        if (index < files.length) {
-                            file = files[index]
-
-                            iterate()
-
-                            return
-                        }
-
-                        callback(new AuthError('Email and/or password wrong'))
-
-                    })
-
-                })()
-            } catch (error) {
-                callback(new UnknownError(error.message))
-            }
+    
+    try{
+        const found = await User.findOne({
+            'email': email,
+            'password': password
         })
 
-    } catch (error) {
+        if(found) return callback(null, found.id) 
+        
+        callback(new AuthError('Email and/or password wrong'))
+    }catch(error){
         callback(new UnknownError(error.message))
     }
 }
--- staff/adrian-ruiz/postits/api/logic/registerUser.js
+++ staff/adrian-ruiz/postits/api/logic/registerUser.js
@@ -1,114 +1,27 @@
-const { writeFile, readdir, readFile } = require('fs')
-const { SystemError, UnknownError, DuplicityError } = require('errors')
+const { UnknownError, DuplicityError } = require('errors')
 const { validateName, validatePassword, validateEmail, validateCallback } = require('validators')
+const User  = require('../models/user')
 
 
-function registerUser(name, email, password, callback) {
+async function registerUser(name, email, password, callback) {
     //TODO validate inputs
     validateName(name)
     validatePassword(password)
     validateEmail(email)
     validateCallback(callback)
+    try{
+        
+        const found = await User.findOne({'email': email})
 
-    const folder = './data/users'
+        if(found) return callback(new DuplicityError(`User with email ${email} already exists`))
+    
+        await User.create({name, email, password})
 
-    try {
-        readdir(folder, (error, files) => {
-            try {
-                if (error) {
-                    debugger
-                    callback(new SystemError(`Can't list files from folder: ${folder}`))
+        callback(null)
 
-                    return
-                }
-
-                files = files.filter(file => !file.startsWith('.'))
-
-                if (files.length) {
-                    let index = 0
-                    let file = files[index];
-
-                    (function iterate() {
-                        readFile(`./data/users/${file}`, 'utf8', (error, json) => {
-                            try {
-                                if (error) {
-                                    callback(new SystemError(`Can't read file: ${file} in folder: ${folder}`))
-
-                                    return
-                                }
-
-                                const user = JSON.parse(json)
-
-                                if (user.email === email) {
-                                    callback(new DuplicityError(`User with email ${email} already exists`))
-
-                                    return
-                                }
-
-                                index++
-
-                                if (index < files.length) {
-                                    file = files[index]
-
-                                    iterate()
-
-                                    return
-                                }
-
-                                const newUser = {
-                                    id: `user-${Date.now()}`,
-                                    name,
-                                    email,
-                                    password,
-                                }
-
-                                const newJson = JSON.stringify(newUser)
-
-                                writeFile(`${folder}/${newUser.id}.json`, newJson, "utf8", (error) => {
-                                    if (error) {
-                                        callback(new SystemError(`Can't write file ${newUser.id}.json in folder: ${folder}`))
-
-                                        return
-                                    }
-
-                                    callback(null)
-                                })
-                            } catch (error) {
-                                callback(new UnknownError(error.message))
-                            }
-                        })
-                    })() // iife
-
-                    return
-
-                }
-                // IF !LENGTH THEN DO THIS CODE
-                const newUser = {
-                    id: `user-${Date.now()}`,
-                    name,
-                    email,
-                    password,
-                }
-
-
-                const newJson = JSON.stringify(newUser)
-
-                writeFile(`${folder}/${newUser.id}.json`, newJson, "utf8", (error) => {
-                    if (error) {
-                        callback(new SystemError(`Can't write file ${newUser.id}.json in folder: ${folder}`))
-
-                        return
-                    }
-
-                    callback(null)
-                })
-
-            } catch (error) {
-                callback(new UnknownError(error.message))
-            }
-        })
-    } catch (error) {
+    }catch(error){
         callback(new UnknownError(error.message))
     }
+    
 }
 module.exports = registerUser
