3e71c3cd76570c2b7d37f2678bae243a16c1bbb3
yoferpatlv <yoferpa_1995@hotmail.com>
2022-08-29 15:54:49 +0200

    errors apptienda

--- staff/jose-fernandez/app.v6tienda/pages/HomePage.jsx
+++ staff/jose-fernandez/app.v6tienda/pages/HomePage.jsx
@@ -1,17 +1,16 @@
 const {useState,useEffect} = React
 function HomePage(){
-    const logger = new Loggito('HomePage')
+    // const logger = new Loggito('HomePage')
 
-    const [view,setView]= useState('products')
+    const [view,setView]= useState('list')
 
     return <div className="container home_page ">
             <Header name={name} onLogoutClick={onLogoutClick} onSettingsClick={handleSettingsClick} view={view} />
 
-            <main className="main_home">
+            {/* <main className="main_home">
                 {view === 'list' && <NoteList notes={notes} onUpdateNote={handleUpdateNote} onDeleteNote={handleDeleteNote} />}
-                {view === 'newNote' && <NewNoteForm onArrowLeft={handleArrowLeftClick} onCloseClick={handleReturnNoteList} onFeedback={onFeedback}/>}
-                {view === 'settings' && <Settings onCloseClick={handleReturnNoteList} email={email} onUpdateEmail={handleUpdateEmail} onFeedback={onFeedback} onUpdateName={handleUpdateName}/>}
-            </main>
+                
+            </main> */}
 
             <footer className="footer_home">
                 {view === 'list' && <div className="btn_plus" onClick={handleAddClick}>
--- staff/jose-fernandez/app.v6tienda/pages/RegisterPage.jsx
+++ staff/jose-fernandez/app.v6tienda/pages/RegisterPage.jsx
@@ -1,3 +1,34 @@
 function RegisterPage(){
-    
+    return  <main className="register_page container">
+    <div className="header_only">
+        <div className="cont__header">
+          <div className="linkhomeRegister">
+            <img className="img img-home " src="https://i.postimg.cc/ZY1nHGSz/logo-Luanna.png" alt=""/>
+        </div>
+            
+        </div>
+    </div>
+    <form className="form form-register">
+
+        <img className="img img-register" src="https://th.bing.com/th/id/OIP.8oRhQutdRIwwPM6f-eTiwwAAAA?pid=ImgDet&rs=1"
+            alt=""/>
+        <div className="form__field">
+            <label className="label__movil">USERNAME</label>
+            <input className="input input-user" type="text" name="name" placeholder="Username"/>
+        </div>
+        <div className="form__field">
+            <label className="label__movil">EMAIL</label>
+            <input className="input input-email" type="email" name="email" placeholder="Email"/>
+        </div>
+        <div className="form__field">
+            <label className="label__movil">PASSWORD</label>
+            <input className="input input-pass" type="password" name="password" placeholder="Password"/>
+        </div>
+
+        <button className="button-login" type="submit">SIGNUP</button>
+
+    </form>
+
+    <a className="link link-login" href="#">LOGIN</a>
+</main>
 }
