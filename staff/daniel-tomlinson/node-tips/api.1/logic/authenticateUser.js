2d5e84623b6228b5d6bf28f7cba85c7f7f561c6a
donyol <danieltomlinson123@gmail.com>
2022-08-31 14:18:52 +0200

    api with node and express initial commit

---
+++ staff/daniel-tomlinson/node-tips/api.1/logic/authenticateUser.js
@@ -0,0 +1,67 @@
+const { readdir, readFile } = require("fs");
+const { AuthError, SystemError, UnknownError } = require("../errors");
+const {
+  validateEmail,
+  validatePassword,
+  validateCallback,
+} = require("../validators");
+
+function authenticateUser(email, password, callback) {
+  validateEmail(email);
+  validatePassword(password);
+  validateCallback(callback);
+
+  const folder = "./data/users";
+
+  /*   api.post("/api/users/auth", jsonBodyParser, (req, res) => {
+    const {
+      body: { email, password },
+    } = req; */
+
+  readdir(folder, (error, files) => {
+    if (error)
+      return callback(
+        new SystemError(`cannot list files from folder ${folder}`)
+      );
+
+    //   res.status(500).json({ error: error.message });
+    files = files.filter((file) => !file.startsWith("."));
+
+    if (files.length) {
+      let index = 0;
+      let file = files[index];
+
+      (function iterate() {
+        readFile(`${folder}/${file}`, "utf8", (error, json) => {
+          if (error)
+            return callback(
+              new SystemError(`cannot read file ${file} in folder ${folder}`)
+            );
+
+          const user = JSON.parse(json);
+
+          if (user.email === email)
+            if (user.password === password) return callback(null, user.id);
+            else return callback(new AuthError("wrong credentials"));
+
+          index++;
+
+          if (index < files.length) {
+            file = files[index];
+
+            iterate();
+
+            return;
+          }
+
+          callback(new AuthError("wrong credentials"));
+        });
+      })(); // iife
+
+      return;
+    }
+    callback(new AuthError("wrong credentials"));
+  });
+}
+
+module.exports = authenticateUser;
