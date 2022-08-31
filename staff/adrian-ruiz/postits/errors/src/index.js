3b9bc8bd5b553af41775364200387c44179bcf2c
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-08-31 20:20:36 +0200

    add notFoundError

--- staff/adrian-ruiz/postits/errors/src/index.js
+++ staff/adrian-ruiz/postits/errors/src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     AuthError: require('./AuthError'),
+    NotFoundError: require('./NotFoundError'),
     DuplicityError: require('./DuplicityError'),
     RegexError: require('./RegexError'),
     FormatError: require('./FormatError'),
