14cb8eaf1d8354832c6a8a9116784aa3fc47df62
donyol <danieltomlinson123@gmail.com>
2022-08-31 14:21:49 +0200

    api with node and express v 3 with create note, retrieve user updated and retrieve notes in progress

---
+++ staff/daniel-tomlinson/node-tips/api.3/logic/registerUser.js
@@ -0,0 +1,25 @@
+const { User } = require("../models");
+const { DuplicityError, SystemError } = require("../errors");
+const {
+  validateText,
+  validateEmail,
+  validatePassword,
+} = require("../validators");
+
+function registerUser(name, email, password) {
+  debugger;
+  validateText(name, "name");
+  validateEmail(email);
+  validatePassword(password);
+
+  return User.create({ name, email, password })
+    .then((user) => {})
+    .catch((error) => {
+      debugger;
+      if (error.code === 11000) throw new DuplicityError("user already exists");
+
+      throw new SystemError(error.message);
+    });
+}
+
+module.exports = registerUser;
