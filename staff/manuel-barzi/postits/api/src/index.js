29402425365c8138e564a1cf746e1a36cf8d2f69
manuelbarzi <manuelbarzi@gmail.com>
2022-08-31 10:56:09 +0200

    update create note; update api

--- staff/manuel-barzi/postits/api/src/index.js
+++ staff/manuel-barzi/postits/api/src/index.js
@@ -1,7 +1,12 @@
 const { connect, disconnect } = require('mongoose')
 const express = require('express')
 const { DuplicityError, NotFoundError, AuthError, FormatError, SystemError } = require('./errors')
-const { registerUser, authenticateUser, retrieveUser } = require('./logic')
+const { 
+    registerUser, 
+    authenticateUser, 
+    retrieveUser, 
+    createNote 
+} = require('./logic')
 const logger = require('./logger')(module)
 const { sign, verify, JsonWebTokenError, TokenExpiredError, NotBeforeError } = require('jsonwebtoken')
 
@@ -84,6 +89,42 @@ connect('mongodb://localhost:27017/postits')
                     .catch(error => {
                         if (error instanceof NotFoundError || error instanceof AuthError)
                             res.status(401).json({ error: 'wrong credentials' })
+                        else
+                            res.status(500).json({ error: 'system error' })
+
+                        logger.error(error)
+
+                        return
+                    })
+            } catch (error) {
+                if (error instanceof TypeError || error instanceof FormatError)
+                    res.status(400).json({ error: error.message })
+                else if(error instanceof JsonWebTokenError || error instanceof TokenExpiredError || error instanceof NotBeforeError)
+                    res.status(401).json({ error: 'token not valid'})
+                else
+                    res.status(500).json({ error: 'system error' })
+
+                logger.error(error)
+            }
+        })
+
+        api.post('/api/notes', jsonBodyParser, (req, res) => {
+            try {
+                const { headers: { authorization } } = req
+
+                const token = authorization.substring(7)
+
+                const payload = verify(token, 'Dan: copié el código de Mónica!')
+
+                const { sub: userId } = payload
+
+                const { body: { text } } = req
+
+                createNote(userId, text)
+                    .then(() => res.status(201).send())
+                    .catch(error => {
+                        if (error instanceof NotFoundError)
+                            res.status(404).json({ error: error.message })
                         else
                             res.status(500).json({ error: 'system error' })
 
