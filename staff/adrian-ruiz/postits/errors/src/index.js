4af7a2553753201404040933e31d0d26db1dfa0e
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-09-01 12:10:24 +0200

    add NotFound404Error (extends NotFoundError) to be more specific

--- staff/adrian-ruiz/postits/errors/src/index.js
+++ staff/adrian-ruiz/postits/errors/src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     AuthError: require('./AuthError'),
     NotFoundError: require('./NotFoundError'),
+    NotFoundError404: require('./NotFoundError404'),
     DuplicityError: require('./DuplicityError'),
     RegexError: require('./RegexError'),
     FormatError: require('./FormatError'),
