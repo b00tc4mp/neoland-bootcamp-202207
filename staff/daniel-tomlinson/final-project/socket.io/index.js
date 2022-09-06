48fad483e0f16068888fecea4fa405dae8d6ae85
donyol <danieltomlinson123@gmail.com>
2022-09-06 14:13:40 +0200

    socket.io v2 localhost bug fixed

--- staff/daniel-tomlinson/final-project/socket.io/index.js
+++ staff/daniel-tomlinson/final-project/socket.io/index.js
@@ -21,20 +21,103 @@ io.on("connection", (socket) => {
     socket.join(data);
   });
 
-  socket.on("1", (data) => {
-    console.log("game code generated");
+  socket.on("T1", (data) => {
+    console.log("T1 server");
     console.log(data);
-    // socket.broadcast.emit("1.5", data);
-    socket.emit("1.5", data);
+    socket.broadcast.emit("T1.5", data);
   });
 
-  socket.on("send_message", (data) => {
+  /*   socket.on("T2", (data) => {
+    console.log("T2 server");
+    console.log(data);
+    socket.broadcast.emit("T2.5", data);
+  });
+ */
+  socket.on("T3", (data) => {
+    console.log("T3 server");
+    console.log(data);
+    socket.broadcast.emit("T3.5", data);
+  });
+
+  socket.on("T4", (data) => {
+    console.log("T4 server");
+    console.log(data);
+    socket.broadcast.emit("T4.5", data);
+  });
+
+  socket.on("T5", (data) => {
+    console.log("T5 server");
+    console.log(data);
+    socket.broadcast.emit("T5.5", data);
+  });
+
+  socket.on("T6", (data) => {
+    console.log("T6 server");
+    console.log(data);
+    socket.broadcast.emit("T6.5", data);
+  });
+
+  /*   socket.on("T7", (data) => {
+    console.log("T7 server");
+    console.log(data);
+    socket.broadcast.emit("T7.5", data);
+  }); */
+
+  socket.on("S1", (data) => {
+    console.log("S1 server");
+    console.log(data);
+    socket.broadcast.emit("S1.5", data);
+  });
+
+  /*   socket.on("S2", (data) => {
+    console.log("S2 server");
+    console.log(data);
+    socket.broadcast.emit("S2.5", data);
+  }); */
+
+  /* socket.on("S3", (data) => {
+    console.log("S3 server");
+    console.log(data);
+    socket.broadcast.emit("S3.5", data);
+  }); */
+
+  socket.on("S4", (data) => {
+    console.log("S4 server");
+    console.log(data);
+    socket.broadcast.emit("S4.5", data);
+  });
+
+  /* socket.on("S5", (data) => {
+    console.log("S5 server");
+    console.log(data);
+    socket.broadcast.emit("S5.5", data);
+  }); */
+
+  /* socket.on("S6", (data) => {
+    console.log("S6 server");
+    console.log(data);
+    socket.broadcast.emit("S6.5", data);
+  }); */
+
+  /* socket.on("S7", (data) => {
+    console.log("S7 server");
+    console.log(data);
+    socket.broadcast.emit("S7.5", data);
+  }); */
+
+  /* socket.on("S8", (data) => {
+    console.log("S8 server");
+    console.log(data);
+    socket.broadcast.emit("S8.5", data);
+  }); */
+
+  /* socket.on("send_message", (data) => {
     console.log("server received:");
     console.log(data);
     console.log(data.room);
-    socket.broadcast.emit("received_message", data);
-    // socket.to(data.room).emit("received_message", data);
-  });
+    socket.broadcast.emit("received_message", data); */
+  // socket.to(data.room).emit("received_message", data);
+  // });
   //   socket.on("send_message", (data) => {
   //     console.log(data);
   //     socket.broadcast.emit("received_message", data);
