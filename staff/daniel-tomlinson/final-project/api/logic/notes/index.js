ba80fe164b8b09b53b9be4abbdb274a821444a23
donyol <danieltomlinson123@gmail.com>
2022-09-20 16:10:39 +0200

    refactoring and code cleanup of project - useRef implementation

--- staff/daniel-tomlinson/final-project/api/logic/notes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  createNote: require("./createNote"),
-  retrieveNotes: require("../questions/retrieveQuestions"),
-  updateNoteText: require("../questions/updateQuestionText"),
-  searchNotes: require("../questions/searchQuestions"),
-  deleteNote: require("../questions/deleteQuestion"),
-};
